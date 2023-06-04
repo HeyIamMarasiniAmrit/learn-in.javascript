b698f4141572aa51dec0b0758a234191f8585c7e
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2023-06-04 12:16:57 +0545

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
-console.log("Hello world!");
-let a = 67
+// var, let, const
+// var
+var a = 10
+function f() {
+    console.log(a)
+}
+f();
+console.log(a);
+
+
+//let
+let a = 10;
+function f() {
+    if (true) {
+        let b = 9
+ 
+        // It prints 9
+        console.log(b);
+    }
+ 
+    // It gives error as it
+    // defined in if block
+    console.log(b);
+}
+f()
+ 
+// It prints 10
 console.log(a)
-a = "harry"
-console.log(a)
-alert("me");
+
+//const
+const a = 10;
+function f() {
+    a = 9
+    console.log(a)
+}
+f();
