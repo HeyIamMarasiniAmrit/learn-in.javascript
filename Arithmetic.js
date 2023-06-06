5391c3caec8baa6b7bfb1983a57796fc1573cd0d
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2023-06-06 20:59:55 +0545

    Add files via upload

---
+++ Arithmetic.js
@@ -0,0 +1,51 @@
+/*let x = 100 + 50;
+document.getElementById("demo").innerHTML = x;
+
+Adding
+The addition operator (+) adds numbers:
+let x = 5;
+let y = 2;
+let z = x + y;
+
+Subtracting
+The subtraction operator (-) subtracts numbers.
+let x = 5;
+let y = 2;
+let z = x - y;
+
+Multiplying
+The multiplication operator (*) multiplies numbers.
+let x = 5;
+let y = 2;
+let z = x * y;
+
+Dividing
+The division operator (/) divides numbers.
+let x = 5;
+let y = 2;
+let z = x / y;
+
+Remainder
+The modulus operator (%) returns the division remainder.
+let x = 5;
+let y = 2;
+let z = x % y;
+
+Incrementing
+The increment operator (++) increments numbers.
+let x = 5;
+x++;
+let z = x;
+
+Decrementing
+The decrement operator (--) decrements numbers.
+let x = 5;
+x--;
+let z = x;
+
+Exponentiation
+The exponentiation operator (**) raises the first operand to the power of the second operand.
+let x = 5;
+let z = x ** 2;
+
+*/
---
+++ Operators.js
@@ -0,0 +1,32 @@
+/*Types of JavaScript Operators
+There are different types of JavaScript operators:
+
+Arithmetic Operators
+Assignment Operators
+Comparison Operators
+String Operators
+Logical Operators
+Bitwise Operators
+Ternary Operators
+Type Operators */
+ // exampale
+
+ /*
+let a = 3;
+let x = (100 + 50) * a;
+document.getElementById("demo").innerHTML = x;
+
+var x = 10;
+x += 5;
+document.getElementById("demo").innerHTML = x;
+
+ */
+
+// Assign the value 5 to x
+let x = 5;
+// Assign the value 2 to y
+let y = 2;
+// Assign the value x + y to z
+let z = x + y;
+// Display z
+document.getElementById("demo").innerHTML = "The sum of x + y is: " + z;
