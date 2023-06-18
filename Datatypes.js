953d618e03475b270aa2c75747f991927cfc7801
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2023-06-18 16:07:52 +0545

    Add files via upload

---
+++ Datatypes.js
@@ -0,0 +1,34 @@
+//JavaScript has 8 Datatypes
+//1. String
+//2. Number
+//3. Bigint
+//4. Boolean
+//5. Undefined
+//6. Null
+//7. Symbol
+//8. Object
+
+//let x;       // Now x is undefined
+x = 5;       // Now x is a Number
+x = "John";  // Now x is a String
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2023-03-25");
