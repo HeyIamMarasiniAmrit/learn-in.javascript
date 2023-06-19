0b71a032814cf64dc6af4bacba121d1ddbae2907
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2023-06-19 14:42:25 +0545

    Add files via upload

---
+++ Functions.js
@@ -0,0 +1,39 @@
+// function showMessage() {
+//     alert( 'Hello everyone!' );
+//   }
+
+function showMessage() {
+    alert( 'Hello everyone!' );
+  }
+  
+  showMessage();
+  showMessage();
+// second example
+
+
+  function showMessage() {
+    let message = "Hello, I'm JavaScript!"; // local variable
+  
+    alert( message );
+  }
+  
+  showMessage(); // Hello, I'm JavaScript!
+  
+  alert( message ); // <-- Error! The variable is local to the function
+
+  //example 3
+  function checkAge(age) {
+    if (age >= 18) {
+      return true;
+    } else {
+      return confirm('Do you have permission from your parents?');
+    }
+  }
+  
+  let age = prompt('How old are you?', 18);
+  
+  if ( checkAge(age) ) {
+    alert( 'Access granted' );
+  } else {
+    alert( 'Access denied' );
+  }
